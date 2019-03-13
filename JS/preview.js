82007a1817c28949ead75a4d5c073d650ff8882f
BurhanBudak <budak.burhan@gmail.com>
2019-03-13 16:48:48 +0100

    fixed

---
+++ JS/preview.js
@@ -0,0 +1,24 @@
+let flexs = document.querySelectorAll('.flex-item-inner');
+let preview = document.querySelector('.preview');
+let previewImage = preview.querySelector('img');
+let previewClose= preview.querySelector('.close');
+
+flexs.forEach(flexItemInner => flexItemInner.addEventListener('click', (e) => {
+    if (window.matchMedia('(max-width:720px)').matches){
+            flexItemInner.firstElementChild.classList.toggle('front-blur');
+            flexItemInner.lastElementChild.classList.toggle('back-flip');
+    }
+    else{
+            change(e);
+    };
+    })
+);
+function change(e) {
+    let src = e.currentTarget.querySelector('img').src;
+            previewImage.src = src;
+            preview.classList.add('open');
+}
+previewClose.addEventListener('click', close);
+function close() {
+    preview.classList.remove('open');
+}
